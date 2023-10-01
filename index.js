b93adc5b957d7280723e01cc585a87d9e8b0baa6
unknown <Md.Jannatul Ferdous (Emon)>
2023-10-01 12:50:23 +0600

    api created for delete item from myCart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qp55ast.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -50,6 +50,13 @@ async function run() {
             const query = { userEmail: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
+        })
+
+        app.delete("/carts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
+            res.send(result); 
         })
 
         // Send a ping to confirm a successful connection
