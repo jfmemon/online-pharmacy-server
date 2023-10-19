41e67f62e89b01f1bfa12ea9cec9e44197760663
unknown <Md.Jannatul Ferdous (Emon)>
2023-10-20 01:32:07 +0600

    health and wellness delete post api done

--- index.js
+++ index.js
@@ -263,8 +263,21 @@ async function run() {
 
         app.get("/healthAndWellness/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: id }
+            const query = { _id: new ObjectId(id) }
             const result = await healthAndWellnessCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.delete("/healthAndWellness/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await healthAndWellnessCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.post("/healthAndWellness", verifyJWT, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await healthAndWellnessCollection.insertOne(newItem);
             res.send(result);
         })
 
